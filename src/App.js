62ec280dd1eb422f3809584296dcf64e5a1367ec
Qasim Mahmood <qasimmahmood107@gmail.com>
2020-06-11 19:02:44 +0500

    Application Updated

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import './App.css';
 function App() {
   return (
     <div>
-       <h1> Surge Application Expense Tracker </h1>
+       <h1> Surge Application Expense Tracker Again </h1>
     </div>
   );
 }
-
 export default App;
