b8ff836e2c4e28e006fb5893ccf819c7c864b0ea
Qasim Mahmood <qasimmahmood107@gmail.com>
2020-06-12 10:40:18 +0500

    Balance Component Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Title } from './Components/Title';
+import { Balance } from './Components/Balance';
 import './App.css';
 
 function App() {
   return (
     <div>
        <Title />
+       <div class="container">
+         <Balance />
+       </div>
     </div>
   );
 }
